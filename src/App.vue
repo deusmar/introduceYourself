9f7f813bae6745b26ea82800ea5d18cecfedf21b
Deusmar Jr <deusmar.junior@hotmail.com>
2018-11-16 11:37:56 -0200

    Initial commit and Firebase settings

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div v-for="(message,index) in messages" :key="index">
+      <h4>{{ message.title }}</h4>
+      <p>{{ message.text }}</p>
+      <p>{{ message.timestamp }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Firebase from 'firebase'
+import config from './firebase-config.js'
+
+let app = Firebase.initializeApp(config)
+
+let db = app.database()
+let messagesref = db.ref('messages')
+export default {
+  name: 'App',
+  firebase: {
+    messages: messagesref
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
