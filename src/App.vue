30ad0a602dad6a2fcd5698f29d6297dcd9274426
Deusmar Junior <deusmar.junior@hotmail.com>
2018-11-16 22:59:24 -0200

    Bootstrap 4 added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">
-    <div v-for="(message,index) in messages" :key="index">
-      <h4>{{ message.title }}</h4>
-      <p>{{ message.text }}</p>
-      <p>{{ message.timestamp }}</p>
+  <div id="app" class="jumbotron">
+    <div class="container">
+      <h1>Hello! Nice to meet you!</h1>
+      <hr />
+      <form  @submit="addMessage">
+        <div class="form-group">
+         <input class="form-control" maxlength="40" autofocus placeholder="Please introduce yourself :)" v-model="newMessage.title" />
+        </div>
+        <div class="form-group">
+          <textarea class="form-control" placeholder="Leave your message!" rows="3" v-model='newMessage.text'></textarea>
+        </div>
+        <button class="btn btn-primary" type="submit">Send</button>
+      </form>
+      <div class="card-group">
+        <div class="card" v-for="(message,index) in messages" :key="index">
+          <div class="card-block">
+            <h4 class="card-title">{{ message.title }}</h4>
+            <p class="card-text">{{ message.text }}</p>
+            <p class="card-text">
+              <small class="text-muted">Added on {{ message.timestamp }}</small>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,11 +35,33 @@ import config from './firebase-config.js'
 let app = Firebase.initializeApp(config)
 
 let db = app.database()
-let messagesref = db.ref('messages')
+let messagesRef = db.ref('messages')
 export default {
   name: 'App',
   firebase: {
-    messages: messagesref
+    messages: messagesRef
+  },
+  data () {
+    return {
+      newMessage: {
+        title: '',
+        text: '',
+        timestamp: null
+      }
+    }
+  },
+  methods: {
+    addMessage (e) {
+      e.preventDefault()
+      if (this.newMessage.title === '') {
+        return
+      }
+      this.newMessage.timestamp = Date.now()
+      messagesRef.push(this.newMessage)
+      this.newMessage.text = ''
+      this.newMessage.title = ''
+      this.newMessage.timestamp = null
+    }
   }
 }
 </script>
@@ -30,8 +71,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
